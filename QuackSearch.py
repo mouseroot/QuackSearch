f7d718b833a4a54194442cfdb59beece8c1ba620
Mouseroot <mouseroot@mousetech.org>
2014-05-27 11:11:28 -0500

    Revamped QuackSearch.py, Removed class and added safe search toggle settings

--- QuackSearch.py
+++ QuackSearch.py
@@ -1,40 +1,18 @@
-#DuckDuckGo - QuackSearch
 import webbrowser as web
+from optparse import OptionParser
 
-#GET Parameters
-#HTTPS - kh
-#Safe search - kp
-#1 = true
-#-1 = false
-
-#API
-# api.duckduckgo.com/api
-
-
-
-class Quack:
-	#Constructor
-	def __init__(self):
-		self.settings = "kp=-1&"
-		self.ddg = "https://duckduckgo.com/?"
-		self.url = None
-		self.quit_flags = ["quit","stop","end"]
-		self.quit = False
-
-	#Search function
-	def search(self, keywords):
-		self.url = self.ddg + self.settings + "q=" + keywords
-		print "Searching: " + self.url
-		web.open_new_tab(self.url)
-		
-def main():
-	print "Quack Search v0.2"
-	qs = Quack()
-	while qs.quit == False:
-		search = raw_input("Search>")
-		if search.lower() in qs.quit_flags:
-			qs.quit = True
+if __name__ == "__main__":
+	usage = "QuackSearch [Safe Mode] [Search term]"
+	desc = "Opens a browser with the given search"
+	parser = OptionParser(usage=usage,description=desc)
+	parser.add_option("-n","--safe-off",action="store_true",default=False,dest="safe_mode",help="Disables adult content (safe mode)")
+	(options,args) = parser.parse_args()
+	if len(args) > 0:
+		if options.safe_mode:
+			safe_str = "&kp=-1"
 		else:
-			qs.search(search)
-
-main()
+			safe_str = "&kp=1"
+		search_term = args[0]
+		web.open_new_tab("".join(["https://duckduckgo.com/?","q=",search_term,safe_str]))
+	else:
+		parser.print_help()
